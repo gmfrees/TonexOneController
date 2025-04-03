3b6d2312b42302481dc53b3b9fc5100c98417a9a
Greg Smith <n/a>
2025-04-03 19:00:49 +1100

    Fixed issue where the access point would be disabled after disconnect of a client, even when the access point mode was not set to timed. Thanks to various users for reporting and helping to debug.

--- source/main/wifi_config.c
+++ source/main/wifi_config.c
@@ -1069,8 +1069,11 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t e
         client_connected = 0;
         control_set_wifi_status(0);
 
-        ESP_LOGI(TAG, "Wifi config stopping");
-        wifi_kill_all();
+        if (control_get_config_item_int(CONFIG_ITEM_WIFI_MODE) == WIFI_MODE_ACCESS_POINT_TIMED)
+        {
+            ESP_LOGI(TAG, "Wifi config stopping");
+            wifi_kill_all();
+        }
     }
     else if ((event_base == WIFI_EVENT) && (event_id == WIFI_EVENT_STA_START))
     {
