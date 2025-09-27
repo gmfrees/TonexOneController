155c10755cd3c88ce14447c78e1f7ca206e72d4e
Greg Smith <n/a>
2025-09-27 21:58:44 +1000

    Working on EEZ Studio port of UI

--- ui_design_800x480land/src/ui/actions.h
+++ ui_design_800x480land/src/ui/actions.h
@@ -11,6 +11,8 @@ extern void action_next_clicked(lv_event_t * e);
 extern void action_previous_clicked(lv_event_t * e);
 extern void action_show_settings_tab(lv_event_t * e);
 extern void action_effect_toggle(lv_event_t * e);
+extern void action_amp_skin_next(lv_event_t * e);
+extern void action_amp_skin_previous(lv_event_t * e);
 
 
 #ifdef __cplusplus
--- ui_design_800x480land/src/ui/ui.h
+++ ui_design_800x480land/src/ui/ui.h
@@ -21,7 +21,7 @@
 extern "C" {
 #endif
 
-extern const uint8_t assets[5332];
+extern const uint8_t assets[6576];
 
 void ui_init();
 void ui_tick();
